cf4ff4bf066703b0db59fb1e31f312ec1be5cc49
robbbowes <up666724@myport.ac.uk>
2023-05-13 18:25:50 +0200

    Add Docker, DbInitialiser, ConnectionFactory, Dapper queries

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,19 +1,17 @@
 using Movies.Application;
+using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,5 +23,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var dbInitialiser = app.Services.GetRequiredService<DbInitaliser>();
+await dbInitialiser.InitialiseAsync();
 
 app.Run();
