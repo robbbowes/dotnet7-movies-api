b4f1d354e45b31f04ba72e1706931cc1cf170e6a
robbbowes <up666724@myport.ac.uk>
2023-05-15 10:28:02 +0200

    Pass viral cancellation token

--- Movies.Application/Repositories/IMovieRepository.cs
+++ Movies.Application/Repositories/IMovieRepository.cs
@@ -4,11 +4,11 @@ namespace Movies.Application.Repositories;
 
 public interface IMovieRepository
 {
-    Task<bool> CreateAsync(Movie movie); // could switch out for MovieDTO, with mapper on this Application layer to convert in this DTO when using repository
-    Task<Movie?> GetByIdAsync(Guid id); // as above
-    Task<Movie?> GetBySlugAsync(string slug); // as above
-    Task<IEnumerable<Movie>> GetAllAsync(); // as above
-    Task<bool> UpdateAsync(Movie movie); // as above
-    Task<bool> DeleteByIdAsync(Guid id);
-    Task<bool> ExistsByIdAsync(Guid id);
+    Task<bool> CreateAsync(Movie movie, CancellationToken token = default); // could switch out for MovieDTO, with mapper on this Application layer to convert in this DTO when using repository
+    Task<Movie?> GetByIdAsync(Guid id, CancellationToken token = default); // as above
+    Task<Movie?> GetBySlugAsync(string slug, CancellationToken token = default); // as above
+    Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default); // as above
+    Task<bool> UpdateAsync(Movie movie, CancellationToken token = default); // as above
+    Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
+    Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
 }
